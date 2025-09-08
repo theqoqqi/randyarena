c6cde9c6a8dc7f6b0e7495277973ea19bab096f3
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-09-08 08:06:29 +0300

    Исправлено поведение функции ForEachVisibleAbility

--- scripts/vscripts/modules/ability_utils.lua
+++ scripts/vscripts/modules/ability_utils.lua
@@ -148,15 +148,9 @@ function AbilityUtils:ForEachAbilityInfo(heroEntity, callback)
 end
 
 function AbilityUtils:ForEachVisibleAbility(heroEntity, callback)
-    AbilityUtils:ForEachAbility(heroEntity, function(ability, index, name)
-        if not ability:IsHidden() and not ability:IsHiddenAsSecondaryAbility() then
-            local behavior = ability:GetBehavior();
-            local innateBit = 8796093022208;
-            local isInnate = behavior >= innateBit and (behavior % (2 * innateBit)) >= innateBit;
-
-            if not isInnate then
-                callback(ability, index, name);
-            end
+    AbilityUtils:ForEachAbility(heroEntity, function(ability, index, abilityName)
+        if AbilityUtils:IsAbilityInstanceVisible(ability) then
+            callback(ability, index, abilityName);
         end
     end);
 end
